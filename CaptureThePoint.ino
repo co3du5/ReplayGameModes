c42ee65e2147b2bcd10f54a14d37c1322894bc6d
THE3RDTRAINER <philiptraineriii@verizon.net>
2024-07-09 22:08:57 -0400

    Gameplay shoudl function now through serial

--- CaptureThePoint.ino
+++ CaptureThePoint.ino
@@ -21,8 +21,17 @@
 
 Rdm6300 rdm6300;
 
+//Variables
+const int ScoreToWin = 500;
+const int PointsToAddEachTime = 1;
+int OrangeTeamScore = 0;
+int YellowTeamScore = 0;
+int currentlyCaputred = 0;
+
 void setup()
 {
+  OrangeTeamScore = 0;
+  YellowTeamScore = 0;
 	Serial.begin(115200);
 	rdm6300.begin(RDM6300_RX_PIN);
 
@@ -40,9 +49,30 @@ void loop()
     tag_id = rdm6300.get_tag_id();
     if(tag_id == 0xB7A546D)
       Serial.println("Orange Team has Captured the point");
+      currentlyCaputred = 1;
 
     if(tag_id == 0xB7B037F)
       Serial.println("Yellow Team has captured the point");
-
+      currentlyCaputred = 2;
 	delay(10);
+
+  switch(currentlyCaputred) {
+    case 1:
+      OrangeTeamScore += PointsToAddEachTime;
+      if(OrangeTeamScore > ScoreToWin){
+        Serial.println(("Orange Team Wins!"));
+        while(1);
+      }
+      break;
+    case 2:
+      YellowTeamScore += PointsToAddEachTime;
+      if(YellowTeamScore > ScoreToWin){
+        Serial.println(("Yellow Team Wins!"));
+        while(1);
+      }
+      break;
+    default:
+      Serial.println("\nNo one has caputred the point");
+      break;
+  }
 }
